ef2bc39ee509dfdb3ac17c9328f219eb3ac95b1a
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-06-19 23:48:34 -0600

    Update main.go

--- go/05genetic/main.go
+++ go/05genetic/main.go
@@ -8,6 +8,7 @@ import (
     "time"
 )
 
+// absolute function
 func absInt(x int) int {
     if x < 0 {
         return -x
@@ -15,6 +16,7 @@ func absInt(x int) int {
     return x
 }
 
+// average function
 func average(arr []int) float64 {
     sum := 0
     for _, v := range arr {
@@ -23,6 +25,7 @@ func average(arr []int) float64 {
     return float64(sum) / float64(len(arr))
 }
 
+// expected value sort
 func sortedByDistance(pop []int, target int) []int {
     sorted := make([]int, len(pop))
     copy(sorted, pop)
@@ -32,6 +35,7 @@ func sortedByDistance(pop []int, target int) []int {
     return sorted
 }
 
+// genetic algorithm
 func genetic(target int, conv float64) {
     rand.Seed(time.Now().UnixNano())
     population := make([]int, 4)
@@ -49,7 +53,9 @@ func genetic(target int, conv float64) {
         parents := sortedByDistance(population, target)
 
         child1 := (parents[0] + parents[1]) / 2
-        child2 := 2*int(math.Max(float64(parents[0]), float64(parents[1]))) - int(math.Min(float64(parents[0]), float64(parents[1])))
+        maxChild2 := int(math.Max(float64(parents[0]), float64(parents[1])))
+        minChild2 := int(math.Min(float64(parents[0]), float64(parents[1])))
+        child2 := 2 * maxChild2 - minChild2
         child3 := absInt(parents[0] - parents[1])
         child4 := int(float64(parents[0]) * 1.1)
         child5 := int(float64(parents[0]) * 0.9)
@@ -62,7 +68,9 @@ func genetic(target int, conv float64) {
         fmt.Println("New population:", population)
 
         newPopulationAvg := average(population)
-        newPopulationConv := math.Min(populationAvg, newPopulationAvg) / math.Max(populationAvg, newPopulationAvg)
+        numerator := math.Min(populationAvg, newPopulationAvg)
+        denominator := math.Max(populationAvg, newPopulationAvg)
+        newPopulationConv := numerator / denominator
 
         populationAvg = newPopulationAvg
         populationConv = newPopulationConv
@@ -71,6 +79,7 @@ func genetic(target int, conv float64) {
     }
 }
 
+// main execution
 func main() {
     genetic(1234, 1)
 }
