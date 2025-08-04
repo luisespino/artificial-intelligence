df9ea0f377e30b81dc373f0c53202f6a3ae0e17c
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-08-03 18:10:40 -0600

    Update mlp_logic_gates.js

--- 10-ml-neural-network/javascript/mlp_logic_gates.js
+++ 10-ml-neural-network/javascript/mlp_logic_gates.js
@@ -1,37 +1,20 @@
 
 import { MLPClassifier, LabelEncoder, accuracyScore } from 'https://snugml.github.io/js/ml.mjs';
 
-const outlook = ['sunny', 'sunny', 'overcast', 'rain', 'rain', 'rain', 'overcast',
-    'sunny', 'sunny', 'rain', 'sunny', 'overcast', 'overcast', 'rain'];
+const X = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+];
 
-const temperature = ['hot', 'hot', 'hot', 'mild', 'cool', 'cool', 'cool',
-    'mild', 'cool', 'mild', 'mild', 'mild', 'hot', 'mild'];
+const y1 = [0, 1, 1, 1];
+const y2 = [0, 0, 0, 1];
+const y3 = [0, 1, 1, 0];
 
-const humidity = ['high', 'high', 'high', 'high', 'normal', 'normal', 'normal',
-    'high', 'normal', 'normal', 'normal', 'high', 'normal', 'high'];
-
-const windy = ['false', 'true', 'false', 'false', 'false', 'true', 'true',
-    'false', 'false', 'false', 'true', 'true', 'false', 'true'];
-
-const label = ['N', 'N', 'P', 'P', 'P', 'N', 'P', 'N', 'P', 'P', 'P', 'P', 'P', 'N'];
-
-const table = outlook.map((_, i) => [outlook[i], temperature[i], humidity[i], windy[i], label[i]]);
-
-showTable(table);
-
-const encoder = new LabelEncoder();
-
-const encOut = encoder.fitTransform(outlook);
-const encTem = encoder.fitTransform(temperature);
-const encHum = encoder.fitTransform(humidity);
-const encWin = encoder.fitTransform(windy);
-const encLab = encoder.fitTransform(label);
-
-const features = encOut.map((_, i) => [encOut[i], encTem[i], encHum[i], encWin[i]]);
-    
-const model = new MLPClassifier(4, 5, 1);
-model.fit(features, encLab);
-const encYPredict = features.map(f => {
+const model = new MLPClassifier(2, 4, 1);
+model.fit(X, y1);
+let yPredict = X.map(f => {
     const output = model.predict(f);
     if (output.length === 1) {
         // Binaria: redondear
@@ -41,41 +24,69 @@ const encYPredict = features.map(f => {
         return output.indexOf(Math.max(...output));
     }
     });
-const yPredict = encoder.inverseTransform(encYPredict);
 
-
-const accuracy = accuracyScore(encLab, encYPredict);
-
+let accuracy = accuracyScore(y1, yPredict);
 const log = document.getElementById('log');
-log.innerHTML = '<br><br>LabelEncoder:<br>'+JSON.stringify(features, null, 2);
-log.innerHTML += '<br><br>Predict:<br>'+ JSON.stringify(yPredict, null, 2);
-log.innerHTML += '<br><br>AccuracyScore: '+accuracy;
+log.innerHTML = '<br><br>OR: X = '+JSON.stringify(X, null, 2);
+log.innerHTML += '; y = '+JSON.stringify(y1, null, 2);
+log.innerHTML += '; yPredict = '+ JSON.stringify(yPredict, null, 2);
+log.innerHTML += '; AccuracyScore: '+accuracy;
 
-function showTable(table) {
-    let container = document.getElementById('table-container');
 
-    // Crear el elemento de la tabla
-    let tableElement = document.createElement('table');
-
-    // Crear la cabecera de la tabla
-    let header = tableElement.createTHead();
-    let headerRow = header.insertRow();
-    let headers = ['Outlook', 'Temp', 'Humidity', 'Windy', 'Label'];
-    headers.forEach(headerText => {
-        let cell = headerRow.insertCell();
-        cell.textContent = headerText;
+model.fit(X, y2);
+yPredict = X.map(f => {
+    const output = model.predict(f);
+    if (output.length === 1) {
+        // Binaria: redondear
+        return Math.round(output[0]);
+    } else {
+        // Multiclase: elegir la clase con mayor probabilidad
+        return output.indexOf(Math.max(...output));
+    }
     });
 
-    // Crear el cuerpo de la tabla
-    let body = tableElement.createTBody();
-    table.forEach(rowData => {
-        let row = body.insertRow();
-        rowData.forEach(cellData => {
-            let cell = row.insertCell();
-            cell.textContent = cellData;
-        });
+accuracy = accuracyScore(y2, yPredict);
+log.innerHTML += '<br><br>AND: X = '+JSON.stringify(X, null, 2);
+log.innerHTML += '; y = '+JSON.stringify(y2, null, 2);
+log.innerHTML += '; yPredict = '+ JSON.stringify(yPredict, null, 2);
+log.innerHTML += '; AccuracyScore: '+accuracy;
+
+
+model.fit(X, y3);
+yPredict = X.map(f => {
+    const output = model.predict(f);
+    if (output.length === 1) {
+        // Binaria: redondear
+        return Math.round(output[0]);
+    } else {
+        // Multiclase: elegir la clase con mayor probabilidad
+        return output.indexOf(Math.max(...output));
+    }
     });
 
-    // Insertar la tabla en el contenedor
-    container.appendChild(tableElement);
-}
+accuracy = accuracyScore(y3, yPredict);
+log.innerHTML += '<br><br>XOR: X = '+JSON.stringify(X, null, 2);
+log.innerHTML += '; y = '+JSON.stringify(y3, null, 2);
+log.innerHTML += '; yPredict = '+ JSON.stringify(yPredict, null, 2);
+log.innerHTML += '; AccuracyScore: '+accuracy;
+
+
+const modelXOR = new MLPClassifier(2, 5, 1);
+
+modelXOR.fit(X, y3);
+yPredict = X.map(f => {
+    const output = modelXOR.predict(f);
+    if (output.length === 1) {
+        // Binaria: redondear
+        return Math.round(output[0]);
+    } else {
+        // Multiclase: elegir la clase con mayor probabilidad
+        return output.indexOf(Math.max(...output));
+    }
+    });
+
+accuracy = accuracyScore(y3, yPredict);
+log.innerHTML += '<br><br>XOR (extra-neuron): X = '+JSON.stringify(X, null, 2);
+log.innerHTML += '; y = '+JSON.stringify(y3, null, 2);
+log.innerHTML += '; yPredict = '+ JSON.stringify(yPredict, null, 2);
+log.innerHTML += '; AccuracyScore: '+accuracy;    
