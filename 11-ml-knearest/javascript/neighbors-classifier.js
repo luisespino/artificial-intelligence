5dcb5f88977cd5b771618d68a36b61f025335771
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-08-03 18:16:36 -0600

    update knearest

--- 11-ml-knearest/javascript/neighbors-classifier.js
+++ 11-ml-knearest/javascript/neighbors-classifier.js
@@ -1,43 +1,37 @@
-async function fit_predict() {
-    const { KNearestNeighbors, accuracyScore } = await import('https://luisespino.github.io/mlearnjs/mlearn.mjs');
-
-    const X = [
-        [1, 6],
-        [2, 7],
-        [3, 6],
-        [4, 2],
-        [5, 3],
-        [6, 1],
-        [7, 5],
-        [8, 6],
-        [9, 4],
-        [10, 5]
-    ];
-
-    const y = [0, 0, 0, 2, 2, 2, 1, 1, 1, 1 ];
-
-    const table = X.map((fila, i) => [...fila, y[i]]);
-
-    showTable(table);
-
-    const myKNearestNeighbors = await KNearestNeighbors(); 
-    const model = new myKNearestNeighbors();
-
-    model.fit(X, y);
-
-    const yPredict = model.predict(X);
-    
-
-    const myAccuracyScore = await accuracyScore();
-    const accuracy = myAccuracyScore(y, yPredict);
-
-    const log = document.getElementById('log');
-    log.innerHTML += '<br><br>Predict:<br>'+ JSON.stringify(yPredict, null, 2);
-    log.innerHTML += '<br><br>AccuracyScore: '+accuracy;
 
 
+import { KNearestNeighbors, accuracyScore } from 'https://snugml.github.io/js/ml.mjs';
 
-}
+const X = [
+    [1, 6],
+    [2, 7],
+    [3, 6],
+    [4, 2],
+    [5, 3],
+    [6, 1],
+    [7, 5],
+    [8, 6],
+    [9, 4],
+    [10, 5]
+];
+
+const y = [0, 0, 0, 2, 2, 2, 1, 1, 1, 1 ];
+
+const table = X.map((fila, i) => [...fila, y[i]]);
+
+showTable(table);
+
+const model = new KNearestNeighbors();
+model.fit(X, y);
+
+const yPredict = model.predict(X);
+
+const accuracy = accuracyScore(y, yPredict);
+
+const log = document.getElementById('log');
+log.innerHTML += '<br><br>Predict:<br>'+ JSON.stringify(yPredict, null, 2);
+log.innerHTML += '<br><br>AccuracyScore: '+accuracy;
+
 
 function showTable(table) {
     let container = document.getElementById('table-container');
@@ -67,5 +61,3 @@ function showTable(table) {
     // Insertar la tabla en el contenedor
     container.appendChild(tableElement);
 }
-
-fit_predict();
